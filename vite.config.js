f449ba757c95ddf122a7776bbec37fa2ec0a229b
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2025-04-30 01:04:03 -0500

    feat: :sparkles: Estructura del frontend
    Se instalan las librerias necesarias para hacer este trabajo y se organiza el frontend para que funcione correctamente

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+const env = loadEnv("development", process.cwd(), 'VITE')
+
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: env.VITE_PORT_FRONTEND,
+    host: env.VITE_HOSTNAME,
+  }
 })
